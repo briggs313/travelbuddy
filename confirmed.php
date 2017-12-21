4671b98713356c24ff2d3fabd5b8a76b33e743fb
bridget barrett <bridget.p.barrett@gmail.comgit config --global user.email pwdgit config --global user.email bridget.p.barrett@gmail.com>
2017-12-20 21:58:43 -0500

    second travel

---
+++ confirmed.php
@@ -0,0 +1,20 @@
+<?php
+$n=$_POST['name'];
+$e=$_POST['email'];
+$s=$_POST['start'];
+$d=$_POST['destination'];
+$g=$_POST['gender'];
+$p=$_POST['prefgender'];
+echo $n;
+echo $e;
+echo $s;
+echo $d;
+echo $g;
+echo $p;
+
+
+
+
+
+
+?>
